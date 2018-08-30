87f8f69e86098cbc7ebd200be6b995b95baf2017
Bert Spaan <bert.spaan@gmail.com>
2018-08-30 22:03:51 +0200

    animate!

--- mycelium.js
+++ mycelium.js
@@ -2,12 +2,15 @@ const got = require('got')
 const turf = require('@turf/turf')
 const R = require('ramda')
 
+const maxDistance = 2800
+const randomPoints = 14
+
 const point = [
   4.9126,
   52.3673
 ]
 
-const buffered = turf.buffer(turf.point(point), 0.8, {
+const buffered = turf.buffer(turf.point(point), maxDistance / 1000, {
   units: 'kilometers',
   steps: 30
 })
@@ -20,17 +23,33 @@ if (!process.env.MAPBOX_DIRECTIONS) {
 const accessToken = process.env.MAPBOX_DIRECTIONS
 const getUrl = (from, to) => `https://api.mapbox.com/directions/v5/mapbox/walking/${from.join(',')};${to.join(',')}?steps=false&alternatives=true&access_token=${accessToken}&geometries=geojson`
 
-const promises = turf.coordAll(buffered).map((to) => {
+const circleRoutes = turf.coordAll(buffered).map((pointOnCircle) => {
+  const line = turf.lineString([point, pointOnCircle])
+  const to = turf.along(line, (Math.random() * (maxDistance - .1) + .1) / 1000, {
+    units: 'kilometers'
+  }).geometry.coordinates
+
   return got(getUrl(point, to), {
     json: true
   })
 })
 
+
+const randomRoutes = turf.randomPoint(randomPoints, {
+  bbox: turf.bbox(buffered)
+}).features
+  .map((randomPoint) => {
+    // console.log(point, randomPoint.geometry.coordinates)
+    return got(getUrl(point, randomPoint.geometry.coordinates), {
+      json: true
+    })
+  })
+
 const getRoutesFromResponse = (response) => {
   return response.body.routes.map((route) => route.geometry)
 }
 
-Promise.all(promises).then((values) => {
+Promise.all([...circleRoutes, ...randomRoutes]).then((values) => {
   const routes = R.flatten(values.map(getRoutesFromResponse))
 
   console.log(JSON.stringify({
